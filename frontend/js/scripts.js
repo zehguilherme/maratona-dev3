707c6ab33b49a5cac7477cee411b43f98f01b587
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-02-19 02:25:04 -0300

    The frontend folder was removed and its files were transferred to the public folder, except index.html (now present in the root)

--- frontend/js/scripts.js
+++
@@ -1,7 +0,0 @@
-// classList: adicionar classes
-// toggle: função que adiciona ou remove conforme há a existência da classe 'hide' ou não
-
-document.querySelector('header button').addEventListener("click", function() {
-    document.querySelector('section.form').classList.toggle('hide');
-})
-
